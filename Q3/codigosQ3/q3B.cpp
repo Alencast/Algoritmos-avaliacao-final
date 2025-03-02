bd9a1105b3751efafe9578d4fddc75cbf43fc192
Robson Alencastro <90284550+Alencast@users.noreply.github.com>
2025-03-02 04:10:33 +0000

    q3B

---
+++ Q3/codigosQ3/q3B.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//globais
+int n;
+int x;
+
+vector<int> preco;
+vector<int> paginas;
+
+vector<vector<int>> memo;
+
+int livro_dp(int i, int orcamento) {
+    // casos base
+    if (i == n)
+        return 0;
+    
+    if (memo[i][orcamento] != -1)
+        return memo[i][orcamento];
+    
+    // Opção 1: Não comprar o livro 
+    int res = livro_dp(i + 1, orcamento);
+    
+    // Opção 2: Comprar o livro i, se houver orcamento
+    if (orcamento >= preco[i])
+        res = max(res, livro_dp(i + 1, orcamento - preco[i]) + paginas[i]);
+    
+  
+    memo[i][orcamento] = res;
+    return res;
+}
+
+int main() {
+    
+    cin >> n >> x;
+    
+    preco.resize(n);
+    paginas.resize(n);
+    
+    for (int i = 0; i < n; i++) {
+        cin >> preco[i] >> paginas[i];
+    }
+    
+    
+    memo.assign(n, vector<int>(x + 1, -1));//iniciar memo com -1
+    
+    cout << livro_dp(0, x) << endl;
+    
+    return 0;
+}
